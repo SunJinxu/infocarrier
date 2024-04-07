4269e489642d90eea1ef3c24ae9be7c0f3a8f5f2
SunJinxu <sunjinxu1996@gmail.com>
2024-04-07 23:08:11 +0800

    initial maven proj

---
+++ src/main/java/io/github/infocarrier/InfocarrierApplication.java
@@ -0,0 +1,13 @@
+package io.github.infocarrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfocarrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfocarrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/infocarrier/InfocarrierApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.infocarrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfocarrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
